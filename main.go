103f0344dda01a59cad8a6b304fe8ae4159e2284
tangx <uyinn28@gmail.com>
2023-01-27 22:42:27 +0800

    init

--- main.go
+++ main.go
@@ -6,67 +6,172 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
-)
 
-var (
-	inCodeBlock = false
+	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v3"
 )
 
 func main() {
-
-	name := "readme.md"
-
-	fi, err := os.Open(name)
-	if err != nil {
-		fmt.Printf("Error: %s\n", err)
-		return
-	}
-	defer fi.Close()
-
-	buf := bytes.NewBuffer(nil)
-
-	br := bufio.NewReader(fi)
-	for {
-		buf.WriteString("\n")
-
-		line, _, c := br.ReadLine()
-		if c == io.EOF {
-			break
-		}
-
-		if isCodeBlock(line) {
-			inCodeBlock = !inCodeBlock
-		}
-
-		img := mustMatchImage(line, inCodeBlock)
-		if img == nil {
-			buf.Write(line)
-			continue
-		}
-
-		// fmt.Println(img.Title, img.Dest)
-		new := "https://cdn.exampla.com/asdf/afsd/image.png"
-		dest := strings.ReplaceAll(string(line), img.Dest, new)
-
-		// fmt.Println(dest)
-		buf.WriteString(dest)
-
-	}
-
-	err = os.WriteFile("new.readme.md", buf.Bytes(), os.ModePerm)
+	err := root.Execute()
 	if err != nil {
 		panic(err)
 	}
 }
 
-func codeblock() bool {
-	return false
+var root = &cobra.Command{
+	Use: "mdhelper",
+	Run: func(cmd *cobra.Command, args []string) {
+		helper := NewMdHelper(config)
+		// file := `./content/posts/2023/01/26/devopscamp-cobra-interactive-survey.md`
+		// helper.Replace(file)
+		helper.Walk(dir)
+	},
 }
 
 var (
-	imagePattern = `!\[(?P<title>.*)?\]\((?P<dest>.*)\)`
+	config    string
+	dir       string
+	codeblock = false
+)
+
+func init() {
+	root.PersistentFlags().StringVarP(&config, "config", "c", "mdhelper.yaml", "mdhelper 配置")
+	root.Flags().StringVarP(&dir, "dir", "", "./content", "目标目录")
+}
+
+type MdHelper struct {
+	RemoteHost      string `yaml:"remoteHost"`
+	WorkspacePrefix string `yaml:"workspacePrefix"`
+	WorkspaceDir    string `yaml:"workspaceDir"`
+}
+
+func NewMdHelper(config string) *MdHelper {
+	data, err := os.ReadFile(config)
+	if err != nil {
+		panic(err)
+	}
+
+	helper := &MdHelper{}
+	err = yaml.Unmarshal(data, helper)
+	if err != nil {
+		panic(err)
+	}
+
+	return helper
+}
+
+func (md *MdHelper) Walk(dirname string) {
+	entries, err := os.ReadDir(dirname)
+	if err != nil {
+		return
+	}
+
+	for _, entry := range entries {
+		name := filepath.Join(dirname, entry.Name())
+		if entry.IsDir() {
+			md.Walk(name)
+		}
+
+		if !strings.HasSuffix(name, ".md") {
+			continue
+		}
+
+		fmt.Println("replace =>", name)
+		md.Replace(name)
+	}
+
+}
+
+func (md *MdHelper) Replace(mdfile string) {
+	// mdfile = content/posts/2023/01/26/abcdef.md
+
+	newNameSuffix := "mdhelper.md"
+	if strings.HasSuffix(mdfile, newNameSuffix) {
+		return
+	}
+
+	fi, err := os.Open(mdfile)
+	if err != nil {
+		return
+	}
+	defer fi.Close()
+
+	buf := bytes.NewBuffer(nil)
+	br := bufio.NewReader(fi)
+	for {
+		line, _, c := br.ReadLine()
+		if c == io.EOF {
+			break
+		}
+
+		// 判断是否为代码块 ```
+		if isCodeBlock(line) {
+			codeblock = !codeblock
+		}
+
+		// 提取 图片地址
+		img := mustMatchImage(line, codeblock)
+
+		// 如果图片地址为 nil， 则不是图片， 直接写入当前数据
+		if img == nil {
+			md.out(buf, line)
+			continue
+		}
+
+		dest := img.Dest
+		// 如果是图片地址, 且为完全地址
+		if strings.HasPrefix(dest, "https://") || strings.HasPrefix(dest, "http://") {
+			// continue
+			md.out(buf, line)
+			continue
+		}
+
+		// 如果是相对地址, 且以 /static 开头
+		// img = /static/assets/logo/avatar.png
+		if strings.HasPrefix(dest, md.WorkspacePrefix) {
+			dest = strings.TrimPrefix(dest, md.WorkspacePrefix)
+		}
+
+		if strings.HasPrefix(dest, "./") {
+			dir := filepath.Dir(mdfile)
+			imgPath := filepath.Join(dir, dest)
+			imgAbsPath, err := filepath.Abs(imgPath)
+			if err != nil {
+				panic(err)
+			}
+
+			dest = strings.TrimPrefix(imgAbsPath, md.WorkspaceDir)
+		}
+
+		dest = strings.TrimLeft(dest, "/")
+		newURL := fmt.Sprintf("%s/%s", md.RemoteHost, dest)
+		newLine := strings.ReplaceAll(string(line), img.Dest, newURL)
+		md.out(buf, []byte(newLine))
+
+		data := buf.Bytes()
+		name := fmt.Sprintf("%s.%s", mdfile, newNameSuffix)
+		os.WriteFile(name, data, os.ModePerm)
+	}
+}
+
+func (md *MdHelper) out(buf *bytes.Buffer, line []byte) {
+	buf.WriteString("\n")
+	buf.Write(line)
+}
+
+var (
+	codeblockRegex = regexp.MustCompile("```(.*)?")
+)
+
+func isCodeBlock(input []byte) bool {
+	return codeblockRegex.Match(input)
+}
+
+var (
+	imagePattern = `^!\[(?P<title>.*)?\]\((?P<dest>.*)\)`
 	imageRegex   = regexp.MustCompile(imagePattern)
 )
 
@@ -98,12 +203,4 @@ func mustMatchImage(input []byte, codeblock bool) *Image {
 		Title: result["title"],
 		Dest:  result["dest"],
 	}
-}
-
-var (
-	codeblockRegex = regexp.MustCompile("```(.*)?")
-)
-
-func isCodeBlock(input []byte) bool {
-	return codeblockRegex.Match(input)
 }
